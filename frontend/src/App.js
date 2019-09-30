a6363c2e51aef7793fe2a870610e82f5ef42bd59
Amanda <mandinhah8@gmail.com>
2019-09-30 19:10:50 -0300

    criacao do frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container className="">
+    </Container>
+  );
+}
+
+export default App;
